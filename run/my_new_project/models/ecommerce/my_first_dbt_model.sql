3b534239ee9811f583983cb3e6f6ae9e3f485eac
buremba <buremba@users.noreply.github.com>
2021-10-08 19:31:09 +0000

    deploy: 4d5147d9f210387ad2e4ab8886ece13847d3b9b7

--- run/my_new_project/models/ecommerce/my_first_dbt_model.sql
+++ run/my_new_project/models/ecommerce/my_first_dbt_model.sql
@@ -1,16 +1,6 @@
 
-
-  create  table "d2svohmkcr2nvd"."public"."my_first_dbt_model__dbt_tmp"
-  as (
-    /*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-*/
-
-
+  create view "d2svohmkcr2nvd"."public"."my_first_dbt_model__dbt_tmp" as (
+    
 
 with source_data as (
 

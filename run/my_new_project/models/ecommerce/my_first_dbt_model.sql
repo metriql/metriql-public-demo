12d25dea02a16551e4fb21b4dd3d20dd1019356a
buremba <buremba@users.noreply.github.com>
2022-02-08 21:47:20 +0000

    deploy: bc666232952c23211f5331393dc815a282a7e5d0

--- run/my_new_project/models/ecommerce/my_first_dbt_model.sql
+++ run/my_new_project/models/ecommerce/my_first_dbt_model.sql
@@ -14,7 +14,7 @@ select *
 from source_data
 
 /*
-    Uncomment the line below to remove records with null `id` values
+    Uncomment the line below to remove records with null `id` values 
 */
 
 -- where id is not null
